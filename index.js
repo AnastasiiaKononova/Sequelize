1d43fd37ecb24c88fdaa84c596a6f8a22a1aee04
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2025-02-25 17:24:04 +0200

    model methods: create, get, delete

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const app = require('./app');
 
 const server = http.createServer(app);
 
+
 const PORT = 5000;
 
-server.listen(PORT), () => {
-    console.log(`Server is running on port ${PORT}`);
-};
+server.listen(PORT, () => {
+    console.log(`App started on port ${PORT}`);
+})
