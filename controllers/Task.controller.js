9212a49506c97357348dd85b92145ef5aa9b68d1
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2025-02-26 16:54:13 +0200

    Error handling

--- controllers/Task.controller.js
+++ controllers/Task.controller.js
@@ -1,29 +1,33 @@
 const {Task, User} = require('../models');
-
+const NotFoundError = require('../errors/NotFoundError');
 
 module.exports.createTask = async (req, res, next) => {
     try {
         const {body, params: {userId}} = req;
         const task = await Task.create({...body, userId});
-        res.status(201).send({data: task});
-    } catch(e) {
-        res.status()
+        res.status(201).send({data: task})
+    } catch (error) {
+        next(error); // прокидування помилки у обробник
     }
 }
 
 module.exports.getTask = () => {
-    // отримання конкретної таски
+// отримання конкретної таски
 // /users/:userId/tasks/:taskId
 }
 
 module.exports.getAllUserTasks = async (req, res, next) => {
     try {
         const {params: {userId}} = req;
-        const user = await User.findByPk(userId);
-        const allUserTasks = await user.getTasks();
-        res.status(200).send({data: allUserTasks})
+        const user = await User.findByPk(Number(userId));
+        if (user) { 
+            const allUserTasks = await user.getTasks();
+            res.status(200).send({data: allUserTasks})
+        } else {
+            throw new NotFoundError('User not found');
+        }
     } catch(error) {
-
+        next(error)
     }
 }
 
