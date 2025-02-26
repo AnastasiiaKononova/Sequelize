94a06ef0d861870046d23bd32a6b7d791f12e9c3
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2025-02-26 15:15:18 +0200

    countTasks resolve

--- controllers/Task.controller.js
+++ controllers/Task.controller.js
@@ -7,11 +7,14 @@ module.exports.createTask = async (req, res, next) => {
         const task = await Task.create({...body, userId});
         res.status(201).send({data: task});
     } catch(e) {
-
+        res.status()
     }
 }
 
-module.exports.getTask = () => {}
+module.exports.getTask = () => {
+    // отримання конкретної таски
+// /users/:userId/tasks/:taskId
+}
 
 module.exports.getAllUserTasks = async (req, res, next) => {
     try {
@@ -26,4 +29,19 @@ module.exports.getAllUserTasks = async (req, res, next) => {
 
 module.exports.updateTask = () => {}
 
-module.exports.deleteTask = () => {}
+module.exports.deleteTask = () => {
+    // видалення конкретної таски
+}
+
+module.exports.countUserTasks = async (req, res, next) => {
+    try {
+        const {params: {userId}} = req;
+        // зробити за допомггою магічного методу 
+        // user.countTasks()
+        const user = await User.findByPk(Number(userId));
+        const countTasks = await user.countTasks();
+        res.status(200).send({data: countTasks});
+    } catch(error) {
+
+    }
+}
