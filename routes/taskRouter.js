d1b2ba17c6326ef5fb364686d24f481e0ce69b75
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2025-02-28 15:45:01 +0200

    Refactor router

--- routes/taskRouter.js
+++ routes/taskRouter.js
@@ -2,4 +2,11 @@ const express = require('express');
 const TaskController = require('../controllers/Task.controller');
 const taskRouter = express.Router();
 
+
+taskRouter.post('/user/:userId', TaskController.createTask);
+taskRouter.get('/user/:userId', TaskController.getAllUserTasks);
+taskRouter.get('/user/:userId/count', TaskController.countUserTasks);
+taskRouter.put('/:taskId', TaskController.updateTask);
+taskRouter.delete('/:taskId', TaskController.deleteTask);
+
 module.exports = taskRouter;
