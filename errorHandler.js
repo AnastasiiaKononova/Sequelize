315edf003b592e9637d03983cd1dc07d36523c02
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2025-02-28 18:22:24 +0200

    Refactor and testing

--- errorHandler.js
+++ errorHandler.js
@@ -1,4 +1,4 @@
-const {ValidationError} = require('sequelize');
+const {ValidationError, DatabaseError} = require('sequelize');
 const NotFoundError = require('./errors/NotFoundError');
 
 module.exports.errorHandler = async (err, req, res, next) => {
@@ -9,6 +9,15 @@ module.exports.errorHandler = async (err, req, res, next) => {
                 message: err.message
             }})
    }
+
+   if (err instanceof DatabaseError) {
+    return res.status(500).send({
+        errors: {
+            message: err.message
+        }
+    })
+   }
+
 
    if (err instanceof NotFoundError) {
     return res.status(404).send({errors: {
